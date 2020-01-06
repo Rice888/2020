9e9a36ad1fa7028de2bc03b59e8f0c410044e24a
Rice <rice88@outlook.com>
2020-01-06 15:29:02 +0800

    js-strict

---
+++ Javascript/01-js/main.js
@@ -0,0 +1,38 @@
+;(function() {
+    'use strict'
+    let name  = 'whh';
+    let nickname =  name;
+
+    console.log(name + ':' + nickname);
+
+    let name0 , name1 ;
+    name0 = 'lu';
+    name1 = 'su';
+    name0 = 'hu';
+
+    console.log(name0 + '-' + name1);
+
+    const A = 1; //常量是大些 不可修改
+    const name3 = 'xg';
+    const age = 5;
+    const B = 1;
+    const C = 1;
+    const D = 1;
+    const E = 1;
+    const F = 1;
+    const G = 1;
+
+
+
+    console.log(name3);
+    
+    console.log(age * 5);
+    
+    console.log(A);
+    
+
+
+    
+    
+
+})();
