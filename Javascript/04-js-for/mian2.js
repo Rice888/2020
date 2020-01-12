b7d7af65b4807a0199eec430db61162fed96cb51
Rice <rice88@outlook.com>
2020-01-12 22:09:53 +0800

    practice one

--- Javascript/04-js-for/mian2.js
+++ Javascript/04-js-for/mian2.js
@@ -1,20 +1,73 @@
-;(function() {
+; (function () {
     'use strict'
-    
-    function shopping(a) {
-        console.log('clothes');
-        console.log('food');
-        console.log('camera');
-        let games = a();
-        console.log('active'+ games);
-        
-        
+    function eachNumber(A, fn) {
+        for (let i = 0; i < A.length; i++) {
+            let it = A[i];
+            fn(it)
+        }
     }
 
-    function game() {
-        return ' game';
+
+    function evenNumber(A) {
+        let result = [];
+        eachNumber(A, it => {
+            if (it % 2) {
+                return;
+            }
+            result.push(it);
+        });
+        return result;
     }
 
-    shopping(game);
+    function oddNumber(A) {
+        let result = [];
+        eachNumber(A,it => {
+            if(!(it % 2)){
+                return;
+            }
+            result.push(it);
+        });
+        return result;
+      }
+
+    function sum(A) {
+        let sum = 0;
+        eachNumber(A,it => sum += it);
+        return sum;
+      }
+
+    function max(A) {
+        let result;
+        eachNumber(A,it =>{
+            result = it
+            if(it > result) {
+                 result = it;
+              }
+        });
+        return result;
+      }
+
+    function min(A) {
+        let result;
+        eachNumber(A,it => {
+            if(result === undefined) {
+                result = it;
+                return;
+            }
+            if(it < result) {
+                 result = it;
+              }
+              
+            });
+            return result;
+      }
+
+    console.log(evenNumber([1, 2, 3, 4, 5, 6]));
+    console.log(oddNumber([1, 2, 3, 4, 5, 6]));
+    console.log(sum([1, 2, 3, 4, 5, 6, 7]));
+    console.log(max([1, 2, 3, 4, 5, 6,7]));
+    console.log(min([ 2, 3, 4, 5]));
+
+
 
 })();
