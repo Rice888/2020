0c78b2711f0bbca53a2d70f041b412d8f87c511b
Rice <rice88@outlook.com>
2020-06-24 20:12:53 +0800

    062501

---
+++ JavascriptMay/00js/1.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-03 15:20:21
+ * @LastEditTime: 2020-05-03 15:21:47
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /RiceProgram/2020/javascriptMay/00js/1.js
+ */
+;(function() {
+    'use strict';
+    
+})();
